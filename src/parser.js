8280a6015af1a5f253b49184c495cd32994694bc
Will Boyd <will@codersblock.com>
2020-12-20 13:30:44 -0500

    Fix issue where images sometimes not set as cover image

--- src/parser.js
+++ src/parser.js
@@ -122,25 +122,25 @@ function collectScrapedImages(data) {
 }
 
 function mergeImagesIntoPosts(images, posts) {
-	// create lookup table for quicker traversal
-	const postsLookup = posts.reduce((lookup, post) => {
-		lookup[post.meta.id] = post;
-		return lookup;
-	}, {});
-
 	images.forEach(image => {
-		const post = postsLookup[image.postId];
-		if (post) {
+		posts.forEach(post => {
+			let shouldAttach = false;
+
+			// this image was uploaded as an attachment to this post
+			if (image.postId === post.meta.id) {
+				shouldAttach = true;
+			}
+
+			// this image was set as the featured image for this post
 			if (image.id === post.meta.coverImageId) {
-				// save cover image filename to frontmatter
+				shouldAttach = true;
 				post.frontmatter.coverImage = shared.getFilenameFromUrl(image.url);
 			}
-			
-			// save (unique) full image URLs for downloading later
-			if (!post.meta.imageUrls.includes(image.url)) {
+
+			if (shouldAttach && !post.meta.imageUrls.includes(image.url)) {
 				post.meta.imageUrls.push(image.url);
 			}
-		}
+		});
 	});
 }
 
