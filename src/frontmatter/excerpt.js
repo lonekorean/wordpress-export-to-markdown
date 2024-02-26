3e2c6d956e8d7d22b7caf4c899d8e1ef29ec5dd0
Will Boyd <will@codersblock.com>
2024-02-26 14:33:43 -0500

    Minor code style

--- src/frontmatter/excerpt.js
+++ src/frontmatter/excerpt.js
@@ -1,5 +1,4 @@
 // get excerpt, not decoded, newlines collapsed
 module.exports = (post) => {
-	const excerpt = post.data.encoded[1].replace(/[\r\n]+/gm, ' ');
-	return excerpt;
+	return post.data.encoded[1].replace(/[\r\n]+/gm, ' ');
 };
