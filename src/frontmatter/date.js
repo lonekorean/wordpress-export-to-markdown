2135f260c91fa88bd075f295e205ee6aa595eaf3
Will Boyd <will@codersblock.com>
2024-02-27 08:27:52 -0500

    Small tweaks, comments

--- src/frontmatter/date.js
+++ src/frontmatter/date.js
@@ -3,8 +3,9 @@ const luxon = require('luxon');
 const settings = require('../settings');
 
 // get post date, optionally formatted as specified in settings
+// this value is also used for year/month folders, date prefixes, etc. as needed
 module.exports = (post) => {
-	const dateTime = luxon.DateTime.fromRFC2822(post.pubDate[0], { zone: settings.custom_date_timezone || 'utc' });
+	const dateTime = luxon.DateTime.fromRFC2822(post.pubDate[0], { zone: settings.custom_date_timezone });
 
 	if (settings.custom_date_formatting) {
 		return dateTime.toFormat(settings.custom_date_formatting);
